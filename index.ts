4779939d569d950ecc7389219d753ef893f64d45
sonysetiawan97 <sonysetiawanred@gmail.com>
2023-02-22 17:00:24 +0700

    package

--- index.ts
+++ index.ts
@@ -1,22 +1,36 @@
-import { decoderFullPath } from './lib/decode_cwr';
-import { encodeCwr } from './lib/encode_cwr';
-import { CwrEncode, Cwr } from './lib/model/cwr';
-import { CWREncode, CWREncodeResult } from './lib/model/encode/v21';
+const decode = import('./src/decode_cwr');
 
-export const DecodeCwr = async (fullpath: string) => {
+const DecodeCwr = async (fullpath: string) => {
   try {
-    const decode: Cwr = await decoderFullPath(fullpath);
-    return decode;
+    return (await decode).decoderFullPath(fullpath);
   } catch (e) {
     throw e;
   }
 };
 
-export const EncodeCwr = async (data: CWREncode): Promise<CWREncodeResult> => {
-  try {
-    const result: CwrEncode = await encodeCwr(data);
-    return result;
-  } catch (e) {
-    throw e;
-  }
+module.exports = {
+  DecodeCwr,
 };
+
+// import { decoderFullPath } from './lib/decode_cwr';
+// import { encodeCwr } from './lib/encode_cwr';
+// import { CwrEncode, Cwr } from './lib/model/cwr';
+// import { CWREncode, CWREncodeResult } from './lib/model/encode/v21';
+
+// export const DecodeCwr = async (fullpath: string) => {
+//   try {
+//     const decode: Cwr = await decoderFullPath(fullpath);
+//     return decode;
+//   } catch (e) {
+//     throw e;
+//   }
+// };
+
+// export const EncodeCwr = async (data: CWREncode): Promise<CWREncodeResult> => {
+//   try {
+//     const result: CwrEncode = await encodeCwr(data);
+//     return result;
+//   } catch (e) {
+//     throw e;
+//   }
+// };
